300a9415cbbe1bc6c0a69bc29a80168c2eb43ca1
Anmol <anmol@tricog.com>
2017-05-07 12:38:48 +0530

    added ui to show data

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -1,15 +1,49 @@
 'use strict';
 /*jshint node:true, quotmark:false*/
-/*global angular,_*/
+/*global angular,_, confirm*/
 
 angular.module('trantorApp')
     .controller('MainCtrl', function ($scope, $http, baseUrl) {
+        
+        $scope.addNewCustomer = false;
         $scope.getCustomers = function(){
-            $http.get(baseUrl+"/customers").success((data)=>{
+            $scope.editCustomers = [];
+
+            $http.post(baseUrl+"/customers/query").success((data)=>{
                 console.log(data);
                 $scope.customers = data;
             }).error((err)=>{
                 $scope.showError(err, "Error in getting customers");
+            });
+        };
+
+
+        $scope.deleteCustomer = function(id){
+            if(!confirm('Are you sure you want to delete?')){
+                return;
+            }
+
+            $http.delete(baseUrl+"/customers?id="+id).success(()=>{
+
+            }).error((err)=>{
+                $scope.showError(err, "Error in deleting customers");
+            });
+        };
+
+
+        $scope.updateCustomer = function(customer, $index){
+            $http.put(baseUrl+"/customers", customer).success(()=>{
+                $scope.editCustomers[$index] = false;
+            }).error((err)=>{
+                $scope.showError(err, "Error in updating customer");
+            });
+        };
+
+        $scope.addCustomer = function(customer){
+            $http.post(baseUrl+"/customers", customer).success(()=>{
+                $scope.addNewCustomer = false;
+            }).error((err)=>{
+                $scope.showError(err, "Error in adding Customer");
             });
         };
 
